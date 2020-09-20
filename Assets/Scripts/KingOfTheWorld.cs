042e4d07a8cb4f5bba5c0ff00a54a08fbb515ee5
Pedro Braga <phbm.braga@gmail.com>
2020-09-20 12:15:38 -0300

    Singleton test

---
+++ Assets/Scripts/KingOfTheWorld.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingOfTheWorld : MonoBehaviour
+{
+    public static KingOfTheWorld CurrentKing { private set; get; }
+
+    public int Kills;
+
+
+    private void Awake()
+    {
+        if (CurrentKing == null)
+        {
+            Debug.Log("Actual king: "+ CurrentKing);
+
+            Debug.Log("NOW WE HAVE A KING!!!");
+            
+            // This is a reference to actual instance of the class
+            CurrentKing = this;
+
+            Debug.Log(this);
+            Debug.Log("Name of the king: " + this.name);
+            Debug.Log("Kills of the king: " + this.Kills);
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else 
+        {
+            // When going to a scene where the game object is already located on
+            // Destroy the duplicated game object (kill the fake king)
+            Destroy(gameObject);
+
+            Debug.Log("Usurper Killed! AQUI NÃO NENEM!");
+
+            // Kills from duplicated king
+            Debug.Log("Kills of the duplicated king (duplicated instace of KingOfTheWorld): " + this.Kills);
+
+            // Kills from real king (static one)
+            Debug.Log("Kills of the real king (static variable from singleton gameObject: CurrentKing): " + CurrentKing.Kills);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManagerScript : MonoBehaviour
+{
+
+    public Text KillsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        KillsText.text = "KILLS: " + KingOfTheWorld.CurrentKing.Kills.ToString();
+    }
+
+    public void OnToScene1Clicked()
+    {
+        SceneManager.LoadScene("Scene1");
+        KingOfTheWorld.CurrentKing.Kills++;
+    }
+
+    public void OnToScene2Clicked()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
